dc42877faa99ee4a31a341c2e1bd37a7c4ff34c4
RomanKapitanov <106028955+RomanKapitanov@users.noreply.github.com>
2024-07-26 21:56:27 +0300

    Delete SpaceGame/Backend directory

--- SpaceGame/Backend/DbRemote/Query/QueryRegistration.js
+++
@@ -1,7 +0,0 @@
-var QueryRegistration = "USE dbspacegame\
-INSERT INTO users (idUser, nickname)\
-VALUES (${}, '${}') AS new_values\
-ON DUPLICATE KEY UPDATE\
-    nickname = new_values.nickname;";
-
-return QueryRegistration;
